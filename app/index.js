cb210c3f63b6d83e67d55980dd50f06654034d47
Zander Martineau <zander@martineau.tv>
2013-09-29 22:16:04 +0100

    initial commit

---
+++ app/index.js
@@ -0,0 +1,73 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+
+var KickoffGenerator = module.exports = function KickoffGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(KickoffGenerator, yeoman.generators.Base);
+
+KickoffGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  // console.log(this.yeoman);
+  var logger = chalk.magenta('\n\n   ##  ## ######  ####  ##  ##  ####  ###### ######\n   ## ##    ##   ##  ## ## ##  ##  ## ##     ##\n   ####     ##   ##     ####   ##  ## ####   ####\n   ## ##    ##   ##  ## ## ##  ##  ## ##     ##\n   ##  ## ######  ####  ##  ##  ####  ##     ##') + '\n\n   Created by ' + chalk.yellow('@MrMartineau') + ' & ' + chalk.cyan('@dragongraphics') + '\n   http://tmwagency.github.io/kickoff/\n';
+  console.log(logger);
+
+  var prompts = [
+  // {
+  //   type: 'confirm',
+  //   name: 'someOption',
+  //   message: 'Would you like to enable this option?',
+  //   default: true
+  // },
+  {
+    name: 'projectName',
+    message: 'Name this project'
+  },
+  {
+    name: 'devNames',
+    message: 'What are the project developer\'s names?'
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.projectName = props.projectName;
+    this.devNames = props.devNames;
+
+    cb();
+  }.bind(this));
+};
+
+KickoffGenerator.prototype.app = function app() {
+  // this.mkdir(this.projectName);
+  // this.mkdir(this.projectName + '/templates');
+
+  this.directory('scss', 'scss');
+  this.copy('scss/kickoff.scss', 'scss/' + this.projectName + '.scss');
+  this.copy('scss/kickoff-old-ie.scss', 'scss/' + this.projectName + '-old-ie.scss');
+
+  this.directory('js', 'js');
+
+  this.template('_humans.txt', 'humans.txt');
+  this.copy('_index.html', 'index.html');
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+  this.copy('Gruntfile.js', 'Gruntfile.js');
+};
+
+KickoffGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+  this.copy('gitignore', '.gitignore');
+};
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,232 @@
+module.exports = function (grunt) {
+
+	'use strict';
+
+	/* 
+	   Javascript settings - Edit this section
+	   ========================================================================== */
+	/**
+	 * Specify which js files you want to include
+	 */
+	var jsFileList = [
+		'js/helpers/helpers.js',
+		'js/helpers/console.js',
+		'js/script.js'
+	];
+
+	/**
+	 * Specify your output directory
+	 */
+	var distDir = 'js/dist/';
+	
+	/**
+	 * Specify the name of your compiled JS file
+	 * which will be placed in the directory you define above
+	 */
+	var jsFile = 'app.min.js';
+
+	/* ==================== */
+
+	/**
+	 * Project configuration
+	 */
+	grunt.initConfig({
+		pkg: require('./package'),
+
+
+		/**
+		 * JSHint
+		 * https://github.com/gruntjs/grunt-contrib-jshint
+		 * Manage the options inside .jshintrc file
+		 */
+		jshint: {
+			all: jsFileList,
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+
+
+		/**
+		 * Sass compilation
+		 * https://github.com/gruntjs/grunt-contrib-sass
+		 * Separate options for dev and production environments
+		 * Includes kickoff.scss and kickoff-old-ie.scss by default
+		 * Also creates source maps
+		 */
+		sass: {
+			dev: {
+				options: {
+					unixNewlines: true,
+					style: 'expanded',
+					lineNumbers: false,
+					debugInfo : false,
+					precision : 8,
+					sourcemap : true
+				},
+				files: {
+					'css/kickoff.css': 'scss/kickoff.scss',
+					'css/kickoff-old-ie.css': 'scss/kickoff-old-ie.scss'
+				}
+			},
+			production: {
+				options: {
+					style: 'compressed',
+					precision : 8
+				},
+				files: {
+					'css/kickoff.css': 'scss/kickoff.scss',
+					'css/kickoff-old-ie.css': 'scss/kickoff-old-ie.scss'
+				}
+
+			}
+		},
+
+
+		/**
+		 * Uglify
+		 * https://github.com/gruntjs/grunt-contrib-uglify
+		 * Minifies and concatinates your JS
+		 * Also creates source maps
+		 */
+		uglify: {
+			options: {
+				// mangle: Turn on or off mangling
+				mangle: true,
+
+				// beautify: beautify your code for debugging/troubleshooting purposes
+				beautify: false,
+
+				// report: Show file size report
+				report: 'gzip',
+
+				// sourceMap: @string. The location of the source map, relative to the project
+				sourceMap: jsFile + '.map',
+
+				// sourceMappingURL: @string. The string that is printed to the final file
+				sourceMappingURL: '../../'+ jsFile +'.map'
+
+				// sourceMapRoot: @string. The location where your source files can be found. This sets the sourceRoot field in the source map.
+				// sourceMapRoot: 'js',
+
+				// sourceMapPrefix: @integer. The number of directories to drop from the path prefix when declaring files in the source map.
+				// sourceMapPrefix: 1,
+
+			},
+
+			/**
+			 * Use the array at the top of this file to specify which js files you include
+			 */
+			js: {
+				src: jsFileList,
+				dest: distDir + jsFile
+			}
+		},
+
+
+		/**
+		 * Watch
+		 * https://github.com/gruntjs/grunt-contrib-watch
+		 * Watches your scss, js etc for changes and compiles them
+		 */
+		watch: {
+			scss: {
+				files: ['scss/**/*.scss'],
+				tasks: ['sass:dev']
+				// tasks: ['sass:dev', 'autoprefixer:dist', 'csso']
+			},
+
+			js: {
+				files: [
+					'Gruntfile.js',
+					'js/*.js',
+					'js/libs/**/*.js'
+				],
+				tasks: ['uglify']
+			},
+
+			livereload: {
+				options: { livereload: true },
+				files: [
+					'css/*.css'
+				]
+			}
+		},
+
+
+		/**
+		 * Autoprefixer
+		 * https://github.com/ai/autoprefixer
+		 * Auto prefixes your CSS using caniuse data
+		 */
+		autoprefixer: {
+			dist : {
+				options: {
+					// Task-specific options go here - we are supporting
+					// the last 2 browsers, any browsers with >1% market share,
+					// and ensuring we support IE7 + 8 with prefixes
+					browsers: ['last 2 versions', '> 1%', 'ie 8', 'ie 7']
+				},
+				files: {
+					'css/kickoff.prefixed.css': 'css/kickoff.css',
+					'css/kickoff-old-ie.prefixed.css': 'css/kickoff-old-ie.css'
+				}
+			}
+		},
+
+
+		/**
+		 * CSSO
+		 * https://github.com/t32k/grunt-csso
+		 * Minify CSS files with CSSO
+		 */
+		csso: {
+			dist: {
+				files: {
+					'css/kickoff.min.css': ['css/kickoff.prefixed.css'],
+					'css/kickoff-old-ie.min.css': ['css/kickoff-old-ie.prefixed.css']
+				},
+
+			}
+		}
+	});
+
+	// Load some stuff
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-csso');
+
+
+	/**
+	 * Available tasks:
+	   * grunt        : run jshint, uglify and sass:dev
+	   * grunt watch  : run sass:dev, uglify and livereload
+	   * grunt dev    : run jshint, uglify and sass:dev
+	   * grunt deploy : run jshint, uglify and sass:production
+	 */
+
+	/**
+	 * Default task
+	 * run jshint, uglify and sass:dev
+	 */
+	// Default task
+	grunt.registerTask('default', ['jshint', 'uglify', 'sass:dev']);
+
+	
+	/**
+	 * A task for development
+	 * run jshint, uglify and sass:dev
+	 */
+	grunt.registerTask('dev', ['jshint', 'uglify', 'sass:dev']);
+
+	/**
+	 * A task for your production environment
+	 * run jshint, uglify and sass:production
+	 */
+	grunt.registerTask('deploy', ['jshint', 'uglify', 'sass:production']);
+	// grunt.registerTask('production', ['jshint', 'uglify', 'sass:production', 'autoprefixer', 'csso']);
+
+};
