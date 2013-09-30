d14e1dd9f84b0bebb995b50e866cb60ce2dd1992
Zander Martineau <zander@martineau.tv>
2013-09-30 10:47:17 +0100

    chalk shizzle

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
 
 
 var KickoffGenerator = module.exports = function KickoffGenerator(args, options, config) {
