c753a816a3c737cc2609d0490856db5ecd905336
Zander Martineau <zander@martineau.tv>
2014-02-26 06:44:56 +0000

    - remove micro libraries as not tested enough - updated npm dependencies -

--- app/templates/js/_script.js
+++ app/templates/js/_script.js
@@ -4,32 +4,14 @@
 */
 
 // Create a closure to maintain scope of the '$' and <%= jsNamespace.toUpperCase() %>
-<% if (jsLibs == 'jquery' || jsLibs == 'micro') {%>
-;(function (<%= jsNamespace.toUpperCase() %>, $) {
-	<% if (jsLibs == 'micro') {%>/* ==========================================================================
-	   Micro libraries
-	   * Bean    : Events API          - https://github.com/fat/bean
-	   * Bonzo   : DOM utility         - https://github.com/ded/bonzo
-	   * Qwery   : CSS Selector engine - https://github.com/ded/qwery
-	   * domReady: Obvious             - https://github.com/ded/domready
-	   * lodash  : Utility library     - http://lodash.com/
+;(function (<%= jsNamespace.toUpperCase() %><% if (jsLibs == 'jquery') {%>, $<% } %>) {
 
-	   * Ajax not included. Consider adding https://github.com/ded/Reqwest
-	     bower install reqwest
-	   ========================================================================== */<% } %>
-<% } else { %>
-;(function (<%= jsNamespace.toUpperCase() %>) {
-<% } %>
-
-	<% if (jsLibs == 'jquery') {%>$(function() {<% } else if(jsLibs == 'micro') { %>domready(function () {<% } %>
-		// Any globals go here in CAPS (but avoid if possible)
-
-		// follow a singleton pattern
-		// (http://addyosmani.com/resources/essentialjsdesignpatterns/book/#singletonpatternjavascript)
+	<% if (jsLibs == 'jquery') {%>$(function() {<% } %>
 
 		<%= jsNamespace.toUpperCase() %>.Config.init();
 
-	});// END DOC READY
+	<% if (jsLibs == 'jquery') {%>});<% } %>
+
 
 
 	<%= jsNamespace.toUpperCase() %>.Config = {
@@ -53,10 +35,6 @@
 	};
 	*/
 
-<% if (jsLibs == 'micro') {%>
-})(window.<%= jsNamespace.toUpperCase() %> = window.<%= jsNamespace.toUpperCase() %> || {}, bonzo);
-<% } else if (jsLibs == 'jquery') { %>
-})(window.<%= jsNamespace.toUpperCase() %> = window.<%= jsNamespace.toUpperCase() %> || {}, jQuery);
-<% } else { %>
-})(window.<%= jsNamespace.toUpperCase() %> = window.<%= jsNamespace.toUpperCase() %> || {});
-<% } %>
+
+
+})(window.<%= jsNamespace.toUpperCase() %> = window.<%= jsNamespace.toUpperCase() %> || {}<% if (jsLibs == 'jquery') { %>, jQuery<% } %>);
