9a348259a68dd23649bdecda8b75dd5410876321
Zander Martineau <zander@martineau.tv>
2014-10-12 13:57:45 +0100

    statix

--- app/templates/statix/src/helpers/helper-eachupto.js
+++ app/templates/statix/src/helpers/helper-eachupto.js
@@ -1,7 +1,7 @@
 module.exports.register = function (Handlebars, options) {
-  'use strict';
+	'use strict';
 
-  	// Warning: untested code
+		// Warning: untested code
 	Handlebars.registerHelper('each_upto', function(ary, max, options) {
 		if(!ary || ary.length == 0)
 			return options.inverse(this);
