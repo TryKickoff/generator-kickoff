a66a2d2160c6533dd3b5908852f406bf4e507f6d
Zander Martineau <zander@martineau.tv>
2015-05-14 18:30:21 +0100

    fixes broken underscore vars in generator
    /cc @ashleynolan: you need double percent `%%` when otherwise the generator will try to transform them into values

--- app/templates/_grunt-configs/_tests.js
+++ app/templates/_grunt-configs/_tests.js
@@ -25,9 +25,9 @@ module.exports.tasks = {
 			jshintrc: '.jshintrc'
 		},
 		project: [
-			'<%=config.srcDir%>/js/**/*.js',
-			'!<%=config.srcDir%>/js/{libs,helpers}/**/*.js',
-			'!<%=config.srcDir%>/js/**/*.min.js'
+			'<%%=config.srcDir%>/js/**/*.js',
+			'!<%%=config.srcDir%>/js/{libs,helpers}/**/*.js',
+			'!<%%=config.srcDir%>/js/**/*.min.js'
 		]
 	},
 
@@ -57,17 +57,17 @@ module.exports.tasks = {
 	validation: {
 		options: {
 			reset: true,
-			path: '<%=config.testing.validationDir%>/validation/validation-status.json',
-			reportpath: '<%=config.testing.validationDir%>/validation/validation-report.json',
+			path: '<%%=config.testing.validationDir%>/validation/validation-status.json',
+			reportpath: '<%%=config.testing.validationDir%>/validation/validation-report.json',
 
 			// ignores these errors
 			// Error ignored is the chrome frame meta tag, since this is a safely ignored validation error
 			relaxerror: ['A meta element with an http-equiv attribute whose value is X-UA-Compatible must have a content attribute with the value IE=edge.']
 		},
 		files: {
-				src: ['**/*.html',
-							'!*/dist/img/**/*.html',
-							'!node_modules/**/*.html']
+			src: ['**/*.html',
+				'!*/dist/img/**/*.html',
+				'!node_modules/**/*.html']
 		}
 	}
 };
