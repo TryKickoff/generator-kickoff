0227e4d849b907d4aaa59d2b8f69467badf7a741
Zander Martineau <zander@martineau.tv>
2015-09-18 17:40:46 +0100

    update to new browserify script style

--- app/templates/assets/src/js/_script-browserify.js
+++ app/templates/assets/src/js/_script-browserify.js
@@ -1,32 +1,35 @@
-/*	Author: <%= devNames %>
-		<%= projectName %>
-*/
+/**
+ * Project Name: <%= projectName %>
+ * Client:
+ * Author: <%= devNames %>
+ * Company:
+ */
 
-// --------------------------------------------- //
-// DEFINE GLOBAL LIBS                            //
-// --------------------------------------------- //
-<% if (jsLibs.indexOf('jquery1') != -1 || jsLibs.indexOf('jquery2') != -1) {%>window.jQuery = window.$ = require('../../../node_modules/jquery/dist/jquery.js');
-<% } else { %>// Uncomment the line below to expose jQuery as a global object to the usual places
-// window.jQuery = window.$ = require('../../../node_modules/jquery/dist/jquery.js');
-<% } %>
+'use-strict';
 
-// force compilation of global libs that don't return a value.
-require("./helpers/log");
-<% if (shims === true) {%>require("./helpers/shims");<% } %>
-
-//initialise <%= jsNamespace.toUpperCase() %> object
-var <%= jsNamespace.toUpperCase() %> = {};
-
-<%= jsNamespace.toUpperCase() %>.Config = {
-
-	init : function () {
-		console.debug('Kickoff is running');
-
-		// Example module include
-		<%= jsNamespace.toUpperCase() %>.UI = require('./modules/UI');
-		<%= jsNamespace.toUpperCase() %>.UI.init();
-	}
-};
+// npm modules
+var ready = require('lite-ready');<%
+if (jsLibs.indexOf('swiftclick') != -1) {%>
+var SwiftClick = require('swiftclick');<% } %><%
+if (jsLibs.indexOf('trak') != -1) {%>
+var trak = require('trak.js');<% } %><%
+if (jsLibs.indexOf('jquery1') != -1 || jsLibs.indexOf('jquery2') != -1) {%>
+window.jQuery = window.$ = require('jquery');<% } %>
 
 
-<%= jsNamespace.toUpperCase() %>.Config.init();
+// Our own modules
+// var browserifyTest = require('./modules/browserifyTest'); // this is a test module, uncomment to try it
+
+// Bundle global libs that don't return a value
+require('console');<%
+if (shims === true) {%>require("./helpers/shims");<% } %>
+
+// DOM ready code goes in here
+ready(function () {<%
+if (jsLibs.indexOf('trak') != -1) {%>
+	trak.start();<% } %><%
+if (jsLibs.indexOf('swiftclick') != -1) {%>
+	var swiftclick = SwiftClick.attach(document.body);<% } %>
+
+	// browserifyTest(); // this is a test, uncomment this line & the line above to try it
+});
