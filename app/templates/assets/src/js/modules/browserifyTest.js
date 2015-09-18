8ed8d72ea2a71d6e08cbb1116b89d1c5c46e7573
Zander Martineau <zander@martineau.tv>
2015-09-18 17:13:32 +0100

    rename `UI.js` to `browserifyTest.js` and improve

--- app/templates/assets/src/js/modules/browserifyTest.js
+++ app/templates/assets/src/js/modules/browserifyTest.js
@@ -1,13 +1,15 @@
 /*
-	UI.js
+	test.js
 	Example module to show how to include other JS files into you browserify build
 */
 
 // dependencies for this module go here
 // e.g. var $ = require('traversty')
 
-module.exports = {
-	init : function () {
-		console.debug('KO.UI module is being initialised');
-	}
-};
+function init() {
+	doSomething();
+}
+
+function doSomething() {}
+
+module.exports = init;
