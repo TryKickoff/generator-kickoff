9afa1a6a23ea51cd0f42c2c469a90f8d53ba21af
Zander Martineau <zander@martineau.tv>
2016-03-14 17:45:44 +0000

    convert this to ES2015 module format

--- generators/app/templates/assets/src/js/_script-browserify.js
+++ generators/app/templates/assets/src/js/_script-browserify.js
@@ -8,31 +8,32 @@
 'use-strict';
 
 // npm modules
-var ready = require('lite-ready');<%
+import ready from 'lite-ready';<%
 
 if (includeSwiftclick) {%>
-var SwiftClick = require('swiftclick');<%
+import SwiftClick from 'swiftclick';<%
 }
 
 if (includeTrak) {%>
-var trak = require('trak.js');<%
+import trak from 'trak.js';<%
 }
 
 if (includeJquery1 || includeJquery2) {%>
 window.jQuery = window.$ = require('jquery');<% } %>
 
-
-// Our own modules
-// var browserifyTest = require('./modules/browserifyTest'); // this is a test module, uncomment to try it
-
 // Bundle global libs that don't return a value
-require('console');
+import 'console';
+
+// Add your project-specific modules here
+//import moduleTest from './modules/moduleTest';
 
 // DOM ready code goes in here
 ready(function () {<%
 if (includeSwiftclick) {%>
-	var swiftclick = SwiftClick.attach(document.body);<% }
-if (includeTrak) {%>
+	const swiftclick = SwiftClick.attach(document.body);
+	swiftclick.useCssParser(true);<% }
+
+	if (includeTrak) {%>
 	trak.start();<% } %>
 
 	// browserifyTest(); // this is a test, uncomment this line & the line above to try it
